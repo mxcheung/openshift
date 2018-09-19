5f1e788dc882cfa079368945e4dc9b08edcf5b43
max cheung <maxcheung@optusnet.com.au>
2018-09-19 23:11:32 +1000

    Initial setup

--- fileupload/src/main/java/bootwildfly/Application.java
+++ fileupload/src/main/java/bootwildfly/Application.java
@@ -1,19 +1,17 @@
 package bootwildfly;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ServletComponentScan
 @SpringBootApplication
-@ComponentScan("bootwildfly")
-@EnableJpaRepositories("bootwildfly.repo")
-@EntityScan("bootwildfly.model")
+@ComponentScan
+@EnableAutoConfiguration
+//@EnableJpaRepositories("bootwildfly.repo")
+//@EntityScan("bootwildfly.model")
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
