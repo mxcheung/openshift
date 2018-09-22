1784d3b0105988ff3d0979b413fd11ceadc5dae5
max cheung <maxcheung@optusnet.com.au>
2018-09-23 09:32:50 +1000

    Add fileupload service

--- fileuploadconsole/src/main/java/fileuploadconsole/H2Config.java
+++ fileuploadconsole/src/main/java/fileuploadconsole/H2Config.java
@@ -19,10 +19,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class H2Config {
-
+/*
 	@Bean(initMethod = "start", destroyMethod = "stop")
 	public org.h2.tools.Server h2WebConsoleServer() throws SQLException {
 		return org.h2.tools.Server.createWebServer("-web", "-webAllowOthers", "-webDaemon", "-webPort", "8082");
 	}
-
+*/
 }
