bff15e57005f706c2e3ba9f69368ce067f7cb0a1
max cheung <maxcheung@optusnet.com.au>
2018-09-22 23:13:21 +1000

    angularjs

--- fileuploadconsole/src/main/webapp/js/controllers/fileupload/fileupload-controller.js
+++ fileuploadconsole/src/main/webapp/js/controllers/fileupload/fileupload-controller.js
@@ -1,27 +1,11 @@
-app.controller('FileUploadController', function($scope, $location,
-		ListTasksByIdFactory, EditTasksFactory, $route, $routeParams) {
-	// Retrieve selected task
-	var selectedTask = ListTasksByIdFactory.query({
-		id : $routeParams.id
-	});
-	selectedTask.$promise.then(function(result) {
-		// Populate scope variables
-		$scope.editTaskCtrl = $scope.editTaskCtrl || {};
-		$scope.editTaskCtrl.taskname = result.taskname;
-		$scope.editTaskCtrl.performdate = new Date(result.performdate);
-		$scope.editTaskCtrl.category = result.category;
-		$scope.editTaskCtrl.priority = result.priority;
-		$scope.editTaskCtrl.isdone = result.isdone;
-	});
+app.controller('FileUploadController', function($scope, $location, $route, $routeParams) {
 
-	this.editTask = function(editTaskCtrl) {
-		EditTasksFactory.update({
-			id : $routeParams.id
-		}, editTaskCtrl);
-
-		window.setTimeout(function() {
-			$location.path('/');
-		}, 10);
-
-	};
+	
+	$scope.data = {
+			   row1: [ "Sample_PDF.pdf", "application pdf", "Sample PDF file" ],
+			   row2: ["Sample_image.png", "imaghe/png", "Picture file" ],
+			   row3: ["Sample_zip.zip", "application/x-zip-compressed", "Zip file" ]
+			};
+	
+	
 });
