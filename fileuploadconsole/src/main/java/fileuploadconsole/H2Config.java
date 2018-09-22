6fe55b10387af2c3536dfdd8488b48ba9f70fb38
max cheung <maxcheung@optusnet.com.au>
2018-09-23 09:12:24 +1000

    Add fileupload service

---
+++ fileuploadconsole/src/main/java/fileuploadconsole/H2Config.java
@@ -0,0 +1,28 @@
+/*
+ *  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fileuploadconsole;
+
+import java.sql.SQLException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2Config {
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public org.h2.tools.Server h2WebConsoleServer() throws SQLException {
+		return org.h2.tools.Server.createWebServer("-web", "-webAllowOthers", "-webDaemon", "-webPort", "8080");
+	}
+
+}
