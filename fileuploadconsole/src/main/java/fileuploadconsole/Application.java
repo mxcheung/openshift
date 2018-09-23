212ed929b69179e5010df0df9acd8d30d1758a2d
max cheung <maxcheung@optusnet.com.au>
2018-09-23 17:23:10 +1000

    h2

--- fileuploadconsole/src/main/java/fileuploadconsole/Application.java
+++ fileuploadconsole/src/main/java/fileuploadconsole/Application.java
@@ -5,18 +5,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
-@EnableJpaRepositories("fileuploadconsole.repo")
-@EntityScan("fileuploadconsole.model")
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
