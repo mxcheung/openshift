d4339f2e66053c5894add23881d948a03642a90e
max cheung <maxcheung@optusnet.com.au>
2018-09-23 10:20:39 +1000

    Add fileupload service

--- fileuploadconsole/src/main/java/fileuploadconsole/model/FileEntity.java
+++ fileuploadconsole/src/main/java/fileuploadconsole/model/FileEntity.java
@@ -1,7 +1,12 @@
 package fileuploadconsole.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
@@ -12,6 +17,12 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 @Table(name = "fileitem")
 public class FileEntity {
 
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+	
 	private String fileName;
 	private String fileType;
 	private String description;
