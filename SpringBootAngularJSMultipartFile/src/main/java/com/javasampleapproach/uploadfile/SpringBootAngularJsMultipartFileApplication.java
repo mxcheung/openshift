6711fe228c1e20ad9474289d9844799b94d5d00d
max cheung <maxcheung@optusnet.com.au>
2018-09-30 16:10:35 +1000

    Servlet

--- SpringBootAngularJSMultipartFile/src/main/java/com/javasampleapproach/uploadfile/SpringBootAngularJsMultipartFileApplication.java
+++ SpringBootAngularJSMultipartFile/src/main/java/com/javasampleapproach/uploadfile/SpringBootAngularJsMultipartFileApplication.java
@@ -5,19 +5,28 @@ import javax.annotation.Resource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import com.javasampleapproach.uploadfile.storage.StorageService;
 
+
 @SpringBootApplication
-public class SpringBootAngularJsMultipartFileApplication implements CommandLineRunner {
+public class SpringBootAngularJsMultipartFileApplication extends SpringBootServletInitializer
+		implements CommandLineRunner {
 
 	@Resource
 	StorageService storageService;
-    
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(SpringBootAngularJsMultipartFileApplication.class, args);
-    }
-    
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootAngularJsMultipartFileApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootAngularJsMultipartFileApplication.class, args);
+	}
+
 	@Override
 	public void run(String... args) throws Exception {
 		storageService.deleteAll();
