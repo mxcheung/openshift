ea2c6d4adfa211f429264d26127d4290befeb8de
max cheung <maxcheung@optusnet.com.au>
2018-10-07 17:48:23 +1100

    postgres

--- SpringJPA-PostgreSQL/src/main/java/com/springjpa/SpringJpaPostgreSqlApplication.java
+++ SpringJPA-PostgreSQL/src/main/java/com/springjpa/SpringJpaPostgreSqlApplication.java
@@ -4,15 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import com.springjpa.repo.CustomerRepository;
 
 @SpringBootApplication
-public class SpringJpaPostgreSqlApplication implements CommandLineRunner{
+public class SpringJpaPostgreSqlApplication  extends SpringBootServletInitializer  implements CommandLineRunner{
 
 	@Autowired
 	CustomerRepository repository;
 	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringJpaPostgreSqlApplication.class);
+    }
 	public static void main(String[] args){
 		SpringApplication.run(SpringJpaPostgreSqlApplication.class, args);
 	}
